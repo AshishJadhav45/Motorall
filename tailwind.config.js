102871288bddb3efd3be6469496717c8f1ca27e1
JADHAV ASHISH <77589461+AshishJadhav45@users.noreply.github.com>
2024-07-07 02:35:31 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Cambria', 'Times New Roman', 'Times', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+};
